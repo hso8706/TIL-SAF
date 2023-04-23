47e2a649c75722a5811c08b31f7b891a11f4bec6
Ha Jung Ho <ggk14@naver.com>
2023-04-23 21:38:05 +0900

    23.04.23

--- 4월/0421-SpringWebMVC03/homework_HaJungHo/src/main/java/com/ssafy/service/UserServcie.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.service;
-
-import com.ssafy.vo.User;
-
-import java.sql.SQLException;
-
-public interface UserServcie {
-    boolean checkUser(User user, String admin) throws SQLException;
-}
